ef06a53856e8828103cb8e4c1bc5f5eaab4c21f5
Himanshu_Chandra <himanshu.chandra@outlook.com>
2017-01-31 16:00:31 +0530

    validation not touched bug corrected

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ var secrets={
     dbUrl:"mongodb://localhost:27017/wuserdb",
     dbCollection:"wuser2",
     sessionKey:["ggggg","aaaaa","sssssss","hhhhh","ffff","kkkkkk","ttt","aaaa"],
-    reqUrl:"http://localhost:1234",
+    reqUrl:"http://192.168.0.101:1234",
     mongoUrl:'mongodb://localhost:27017/nses',
     defaultSessionDuraion:2*60*60,
     SMTPS_EMAIL:"surveyshimanshu@gmail.com",
