e477baccb8a3b498f88d6cf3fd83524efc230656
Himanshu_Chandra <himanshu.chandra@outlook.com>
2017-02-13 01:30:35 +0530

    bugs fixed

--- config/config.js
+++ config/config.js
@@ -1,3 +1,5 @@
+
+
 var secrets={
     dbUrl:"mongodb://localhost:27017/wuserdb",
     dbCollection:"wuser2",
@@ -5,13 +7,12 @@ var secrets={
     reqUrl:"http://localhost:1234",
     mongoUrl:'mongodb://localhost:27017/nses',
     defaultSessionDuraion:2*60*60,
-    SMTPS_EMAIL:"SendersEmail",
-    SMTPS_PASSWORD:"YourPassword",
+    SMTPS_EMAIL:"Senders Email",
+    SMTPS_PASSWORD:"Password",
     SMTPS_URL:'smtp.gmail.com',
     COMPANY_NAME:'ILUZEX',
-    TWILIO_ACCOUNT_SID:'Twilio account sid',
-    TWILIO_AUTH_TOKEN:'Twilio authentication token',
-    VALID_TWILIO_NUMBER:'Twilio number',
+    TWILIO_ACCOUNT_SID:'twilio account sid',
+    TWILIO_AUTH_TOKEN:'twilio account password',
+    VALID_TWILIO_NUMBER:'twilio account number',
 }
 module.exports=secrets;
-
