894e027ea93c2eecda09def5d98c527638f5c405
Himanshu_Chandra <himanshu.chandra@outlook.com>
2017-06-11 00:14:17 +0530

    validate trim bug fixed

--- config/validate.js
+++ config/validate.js
@@ -3,6 +3,9 @@
 const validate={
 
   username:function(string){
+      if(string===undefined){
+          return false;
+      }
       var string=string.trim();
       var letters = /^[A-Za-z0-9._]+$/;
       if(string.length<5 || string.length>20 || string.match(letters)===null){
@@ -13,6 +16,9 @@ const validate={
       } 
   },
   password:function(string){
+      if(string===undefined){
+          return false;
+      }
       var string=string.trim();
       var letters = /^[a-z0-9]+$/;
       if(string.length!=32 || string.match(letters)===null){
@@ -23,6 +29,9 @@ const validate={
       } 
   },
   email:function(string){
+      if(string===undefined){
+          return false;
+      }
       var string=string.trim();
       var letters = /^[A-Z0-9a-z!@#$%&*+-/=?^_`'{|}~]+$/;
       if(string.length<5 || string.length>50 || string.match(letters)===null || string.match("@")===null){
@@ -33,6 +42,9 @@ const validate={
       }   
   },
   mobile:function(string){
+      if(string===undefined){
+          return false;
+      }
       var string=string.trim();
       var letters = /^[0-9]+$/;
       if(string.length!=10 || string.match(letters)===null){
@@ -43,6 +55,9 @@ const validate={
       }  
   },
   number:function(string){
+      if(string===undefined){
+          return false;
+      }
       var string=string.trim();
       var letters = /^[0-9]+$/;
       if(string.length<3 || string.length>15 || string.match(letters)===null){
@@ -53,6 +68,9 @@ const validate={
       }   
   },
   string:function(string){
+      if(string===undefined){
+          return false;
+      }
       var string=string.trim();
       var letters = /^[A-Za-z0-9-/_',. ]+$/;
       if(string.length<2 || string.length>60 || string.match(letters)===null){
@@ -63,6 +81,9 @@ const validate={
       }  
   },
   name:function(string){
+      if(string===undefined){
+          return false;
+      }
       var string=string.trim();
       var letters = /^[A-Za-z ]+$/;
       if(string.length<3 || string.length>30 || string.match(letters)===null){
@@ -73,6 +94,9 @@ const validate={
       }  
   },
   code:function(string){
+      if(string===undefined){
+          return false;
+      }
       var string=string.trim();
       var letters = /^[A-Za-z0-9+]+$/;
       if(string.length<2 || string.length>16 || string.match(letters)===null){
